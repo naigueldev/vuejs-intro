dbcd868d815ebd20edd8ce6c8a0fc6ca58e518fd
Naiguel Santos <naiguelsantos@MacBook-Pro.local>
2019-11-28 22:10:59 -0300

    vue js forms

--- js/main.js
+++ js/main.js
@@ -1,6 +1,163 @@
+Vue.component('product-review', {
+ template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+    <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name" >
+    </p>
+
+    <p>
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+    </p>
+
+    <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>5</option>
+        <option>2</option>
+        <option>1</option>
+    </select>
+    </p>
+
+    <p>
+    <input type="submit" value="Submit">
+    </p>
+    </form>
+ `,
+ data() {
+     return {
+         name: null,
+         review: null,
+         rating: null
+     }
+ },
+ methods:{
+    onSubmit() {
+        let productReview = {
+            name: this.name,
+            review: this.review,
+            rating: this.rating
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
+    }
+ }
+})
+
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="product">
+            <div class="product-image">
+                <img :src="image" :alt="altText" />
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                
+                <p v-if="inStock">In Stock</p>
+                <p v-else :class="{ outOfStock: !inStock }">Out of stock</p>
+                
+                <p>Shipping: {{ shipping }}</p>
+
+
+                <ul>
+                    <li v-for="detail in details">
+                        {{detail}}
+                    </li>
+                </ul>
+                
+                <div 
+                class="color-box"
+                v-for="(variant, index) in variants" 
+                :key="variant.variantId"
+                :style="{ backgroundColor: variant.variantColor }"
+                @mouseover="updateProduct(index)"
+                >
+            </div>
+            
+           
+            <button @click="addToCart"
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >Add to cart
+            </button>
+        
+            </div>
+    </div>
+
+    `,
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green",
+                    variantImage: "./images/socks-green.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue",
+                    variantImage: "./images/socks-blue.png",
+                    variantQuantity: 1
+                }
+            ]
+        }
+    } ,
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
+        },
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+            return (this.premium) ? "Free" : 2.99
+        }
+    }
+})
+
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        premium: false,
+        cart: [],
+        reviews: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
+        }
+
     }
-});
+})
